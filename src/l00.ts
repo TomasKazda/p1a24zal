b13d1e59ae1f2ef5b50d739addb78337fdf4ab49
Tomáš Kazda <tomas.kazda@pslib.cz>
2024-10-18 13:31:17 +0200

    lesson 24-10-18

---
+++ src/l00.ts
@@ -0,0 +1,13 @@
+//Seznámení s Vite 11.10.2024
+
+//Letos není cílem tvořit webovou aplikaci, ale spíše se zaměřit na TypeScript a jeho možnosti
+//Proto budeme veškeré vstupy a výstupy zobrazovat v konzoli (console.log)
+//Konzoli otevřete klávesovou zkratkou F12 (DevTools)
+//Můžete použít funkci console.clear() pro smazání konzole
+
+//vytvoření nového projektu pomocí Vite a NodeJS:
+//1. nainstalujte NodeJS z https://nodejs.org/
+//2. vytvořte nový projekt pomocí příkazu "npm init vite@latest"
+//3. přejděte do složky projektu pomocí "cd <název složky>"
+//4. nainstalujte závislosti pomocí "npm install"
+//5. spusťte projekt pomocí "npm run dev"
---
+++ src/l01.ts
@@ -0,0 +1,30 @@
+//Základní datové typy v TypeScriptu (https://www.typescriptlang.org/docs/handbook/2/everyday-types.html)
+
+let a: number = 5;
+let b: string = 'Hello';
+let c: boolean = true;
+let d: any = 'Any type';
+const e: number[] = [1, 2, 3];
+const f: Array<number> = [1, 2, 3];
+let g: [string, number] = ['Hello', 5];
+const h: object = { name: 'John', age: 25 }; //object type is not recommended
+let i: null = null;
+let j: undefined = undefined;
+
+type PersonType = {
+    name: string;
+    age: number;
+}
+const person: PersonType = { name: 'John', age: 25 };
+
+console.log("a: number = ", a);
+console.log("b: string = ", b);
+console.log("c: boolean = ", c);
+console.log("d: any = ", d);
+console.log("e: number[] = ", e);
+console.log("f: Array<number> = ", f);
+console.log("g: [string, number] = ", g);
+console.log("h: object = ", h);
+console.log("i: null = ", i);
+console.log("j: undefined = ", j);
+console.log("person: PersonType = ", person);
