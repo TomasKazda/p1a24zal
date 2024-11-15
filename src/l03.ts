5a3fa9b59d84c52a816077e8617e069e28541f60
Tomáš Kazda <tomas.kazda@pslib.cz>
2024-11-15 13:32:06 +0100

    l241115

--- src/l03.ts
+++ src/l03.ts
@@ -19,6 +19,26 @@ console.log(`e = ${a} na ${b} = `, e); //125
 let f: number = Math.pow(a, b); //5 na 3
 console.log(`f = Math.pow(${a}, ${b}) = `, f); //125
 
+//kombinování přiřazení a aritmetických operátorů
+a += b; //a = a + b
+console.log(`a += ${b} = `, a); //8
+a -= b; //a = a - b
+console.log(`a -= ${b} = `, a); //5
+a *= b; //a = a * b
+console.log(`a *= ${b} = `, a); //15
+a /= b; //a = a / b
+console.log(`a /= ${b} = `, a); //5
+a **= b; //a = a ** b
+console.log(`a **= ${b} = `, a); //125
+a %= b; //a = a % b
+console.log(`a %= ${b} = `, a); //2
+
+a += 2; //a = a + 2
+a += 1; //a = a + 1
+a++; //a = a + 1 není to stejné jako a += 1
+++a; //a = a + 1 není to stejné jako a += 1
+
+
 //druhá odmocnina znak Math.sqrt()
 let g: number = Math.sqrt(25); //odmocnina z 25
 console.log(`g = Math.sqrt(25) = `, g); //5
@@ -53,3 +73,31 @@ console.log(`k = ${l} == "${text}" = `, k); //true
 //znak tři rovná se === (porovnává dvě hodnoty) 
 let m: boolean = l === text; //5 === "5"
 console.log(`m = ${l} === "${text}" = `, m); //false
+
+//znak nerovná se != (porovnává dvě hodnoty na základě podobnosti bez ohledu na jenich typ)
+let n: boolean = l != text; //5 != "5"
+
+//znak hodnotově nerovná se !== (porovnává dvě hodnoty)
+let o: boolean = l !== text; //5 !== "5"
+
+//znak větší než > (porovnává dvě hodnoty)
+//znak větší než nebo rovno >= (porovnává dvě hodnoty)
+//znak menší než < (porovnává dvě hodnoty)
+//znak menší než nebo rovno <= (porovnává dvě hodnoty)
+
+//logické operátory
+//boolovské hodnoty = typ boolean: True/False
+
+//znak (a zárověň) && (logický součin)
+let p: boolean = true;
+let q: boolean = false;	
+let r: boolean = p && q; //true && false
+console.log(`r = ${p} && ${q} = `, r); //false
+
+//znak (logické nebo) || (logický součet)
+let s: boolean = p || q; //true && false
+console.log(`s = ${p} || ${q} = `, s); //true
+
+//znak (logický negace) ! (logický negace)
+let t: boolean = !p; //!true
+console.log(`t = !${p} = `, t); //false
